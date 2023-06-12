9ef5799bd3f3d7b084e80ced4a4a4f997aaec8fa
sogood17 <sogoodpiano@gmail.com>
2023-06-12 17:42:11 +0900

    todo input component

---
+++ src/components/CreateTodo.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Todoinput = styled.input`
+    color: green;
+    background-color: white;
+`
+
+const CreateTodo =() => {
+    <div>
+        <Todoinput />
+    </div>
+
+}
+
+export default CreateTodo;
